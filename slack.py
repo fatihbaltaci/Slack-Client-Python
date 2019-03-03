04b4b2fd4a99ff4f4517a75f094024f803ced14a
Fatih Baltacı <baltacifatih14@gmail.com>
2019-03-04 00:05:17 +0300

    response added

--- slack.py
+++ slack.py
@@ -1,10 +1,13 @@
 from slackclient import SlackClient
+import json
 
 slack_token = "<YOUR_SLACK_TOKEN>"
 sc = SlackClient(slack_token)
 
-sc.api_call(
+response = sc.api_call(
   "chat.postMessage",
   channel="#test_channel",
   text="Hello from Python! :tada:"
 )
+
+print(json.dumps(response, indent=4))
